60d9aa333ec369f70cdee4e86bdd552a658a77e5
Fano Raiza <fano@Fanos-MacBook-Pro.local>
2025-01-13 12:39:34 +0300

    Changed minimum password length

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ class Config:
     DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/dbname")
     SMS_GATEWAY_URL = os.getenv("SMS_GATEWAY_URL", "http://127.0.0.1:5001/send-sms")
     JWT_ACCESS_TOKEN_EXPIRES = 30 * 24 * 3600
-    MIN_PASSWORD_LENGTH = 8
+    MIN_PASSWORD_LENGTH = 6
